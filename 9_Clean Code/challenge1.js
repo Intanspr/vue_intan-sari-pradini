5e52be065e2868ea9a867d08d0eb5a206721a257
Intanspr <99669083+Intanspr@users.noreply.github.com>
2022-03-11 15:03:18 +0700

    Update challenge1.js

---
+++ 9_Clean Code/challenge1.js
@@ -0,0 +1,13 @@
+// Problem 1 - Analysis
+
+// ada 5 kekurangan
+
+// Berikut bagian kekurangan dan beberapa alasan
+
+// 1. pada bagian class user sesudahnya kurang ditambahkan constructur
+
+// 2. pada bagian var seharusnya diganti menjadi "this"
+
+// 3. pada baris 8 penempatan "user[]"
+
+// 5. pada baris 10 dan 14 penempatan "user[]" di hapus karena sudah ada getalluserid()
