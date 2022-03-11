746c27a9167e92df6fe6b2d3648372b35a8ca78d
Intanspr <99669083+Intanspr@users.noreply.github.com>
2022-03-11 17:07:22 +0700

    Update challenge1.js

--- 9_Clean Code/challenge1.js
+++ 9_Clean Code/challenge1.js
@@ -4,10 +4,12 @@
 
 // Berikut bagian kekurangan dan beberapa alasan
 
-// 1. pada bagian class user sesudahnya kurang ditambahkan constructur
+// 1. pada bagian class user di instead menggunakan camelCase sehingga menjadi userS dan juga sesudahnya kurang ditambahkan constructur
 
 // 2. pada bagian var seharusnya diganti menjadi "this"
 
-// 3. pada baris 8 penempatan "user[]"
+// 3. pada baris 7 penempatan class user service di instead menggunakan camelCase sehingga menjadi userService
 
-// 5. pada baris 10 dan 14 penempatan "user[]" di hapus karena sudah ada getalluserid()
+// 5. pada baris 10 penempatan getalluser di instead menjadi getUser. (penggunaan instead dengan camelCase karena lebih dari satu kata)
+
+// 6. Pada baris 14 bagian getuserbyid di instead menjadi getUserByid.
