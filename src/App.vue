67c11b3e8aa1e5279875e048833f1a0cd4b68faa
unknown <intanpradini03@gmail.com>
2022-03-19 13:07:33 +0700

    add vue cli

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+     <ol>
+      <li v-for="todo in todos" v-bind:key="todo">
+        {{ todo }}
+      </li>
+    </ol>
+  <input type="text" v-model="item">
+  <button @click="Tambahkan">Tambahkan</button>
+  <div v-if="todos.length >=4">Hebat</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+        return {
+          item: '',
+          todos: []
+        }
+      },
+
+      methods: {
+        Tambahkan() {
+          if(this.item !=''){this.todos.push( this.item )}
+          this.item='';
+        }
+      }, 
+
+}
+</script>
