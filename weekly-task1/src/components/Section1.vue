e169ab0d8408ddbf430fb769dcd8956ebdfefec0
intanspr <intanpradini03@gmail.com>
2022-07-23 00:16:41 +0700

    weekly task

---
+++ weekly-task1/src/components/Section1.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="S2">
+    <h1>Version Control and Branch Management (Git)</h1>
+    <br />
+    <v-toolbar-title>
+      Version Control atau Versioning adalah untuk mengatur versi dari source
+      code program.
+      <br />
+      Git merupakan salah satu version control system pupuler yang digunakan
+      para developer untuk mengembangkan software secara bersama-sama.
+      <br />
+      Pemanfaatan git ini secara distribusi. Git dapat di jalankan pada system
+      operasi macOS,Windows, dan Linux.
+    </v-toolbar-title>
+
+    <h3>
+      Latihan-Version Control and Branch Management (Git)
+      <a
+        href="https://docs.google.com/document/d/1nz3T5hTSC-C1TjsPnCvTCrkfzzJUEan4zOXxthfotTY/edit"
+        target="_blank"
+        >klik disini</a
+      >
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "section1Component",
+  setup() {},
+};
+</script>
